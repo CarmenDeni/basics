ea714fafefe1f6cd1cddaaf77ee4ddce0762bf7a
CarmenDeni <95514587+CarmenDeni@users.noreply.github.com>
2024-04-12 11:42:23 -0600

    Update imports.py

---
+++ imports.py
@@ -0,0 +1,17 @@
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = "all"
+
+import json
+import csv
+import re
+import itertools
+import numpy as np
+import pandas as pd
+pd.set_option('display.max_rows', None)
+
+def flatten(xss):
+    return [x for xs in xss for x in xs]
+
+def json_print(data):
+    print(json.dumps(data, indent=2))
+  
